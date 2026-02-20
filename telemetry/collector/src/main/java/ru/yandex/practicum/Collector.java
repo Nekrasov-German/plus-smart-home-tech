062944f23ba2a84c6b73cbed8bc62b05b0f09448
German Nekrasov <gazelgerman@yandex.ru>
2026-02-20 19:27:43 +0500

    Rename Collector.

--- telemetry/collector/src/main/java/ru/yandex/practicum/Collector.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/Collector.java
@@ -2,11 +2,12 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Collector {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Collector.class, args);
+        SpringApplication.run(Collector.class, args);
     }
 }
